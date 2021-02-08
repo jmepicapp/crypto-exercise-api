ce51792cf6930f514eb7aae8c6ebc7e02f0d0a3b
José María Agüero <jmaguero92@gmail.com>
2021-02-08 04:41:09 +0100

    Account Service implementation

---
+++ src/main/java/com/jmaguero/crypto/exercise/api/service/AccountService.java
@@ -0,0 +1,17 @@
+package com.jmaguero.crypto.exercise.api.service;
+
+import com.jmaguero.crypto.exercise.api.entity.AccountEntity;
+import com.jmaguero.crypto.exercise.api.entity.dto.AccountDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface AccountService {
+
+    AccountDTO find (Integer id);
+
+    AccountDTO save(AccountDTO account);
+
+    AccountDTO update(AccountDTO account, Integer id);
+
+    void delete(Integer id);
+}
---
+++ src/main/java/com/jmaguero/crypto/exercise/api/util/CommonMessage.java
@@ -0,0 +1,6 @@
+package com.jmaguero.crypto.exercise.api.util;
+
+public abstract class CommonMessage {
+
+    public static final String AccountNotFoundMessage = "The account has not been found";
+}
